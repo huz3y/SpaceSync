66692fa4bfb20ada707a88381d5289fe6f5b1322
huz3y <huzey4hassan@gmail.com>
2024-08-27 11:59:57 +0500

    Setup Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Spaces from "./pages/Spaces";
+import CreateUser from "./pages/CreateUser";
+import Settings from "./pages/Settings";
+import UpdateAccount from "./pages/UpdateAccount";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
 export default function App() {
-  return <div>SpaceSync</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Navigate replace to="/dashboard" />} />
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="spaces" element={<Spaces />} />
+        <Route path="create-user" element={<CreateUser />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="update-account" element={<UpdateAccount />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
