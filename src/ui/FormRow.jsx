6e408ab314998657dcd73ed304bf2b7887513b5a
huz3y <huzey4hassan@gmail.com>
2024-08-29 06:57:44 +0500

    reusable form row

---
+++ src/ui/FormRow.jsx
@@ -0,0 +1,29 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+function FormRow({ label, id, error, children }) {
+  const inputId = id || children.props.id;
+
+  return (
+    <div className="form__row">
+      {label && (
+        <label className="form__label" htmlFor={inputId}>
+          {label}
+        </label>
+      )}
+      {React.cloneElement(children, { id: inputId })}
+      {error && <span className="form__error">{error.message}</span>}
+    </div>
+  );
+}
+
+FormRow.propTypes = {
+  label: PropTypes.string,
+  id: PropTypes.string,
+  error: PropTypes.shape({
+    message: PropTypes.string,
+  }),
+  children: PropTypes.node.isRequired,
+};
+
+export default FormRow;
