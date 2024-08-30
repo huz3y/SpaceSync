9c36d2b3c3b7e5bb5aee5c06f3899a5f1ca3fad1
huz3y <huzey4hassan@gmail.com>
2024-08-30 11:37:08 +0500

    api to interact with bookings table

---
+++ services/apiBookings.js
@@ -0,0 +1,99 @@
+import { getToday } from "../src/utils/getToday";
+import supabase from "./supabase";
+
+export async function getBookings() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, spaces(name), clients(full_name, email)");
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to load Bookings!");
+  }
+  return data;
+}
+
+export async function getBooking(id) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, spaces(*), clients(*)")
+    .eq("id", id)
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking not found");
+  }
+
+  return data;
+}
+
+export async function getBookingsAfterDate(date) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("created_at, price, status, paid")
+    .gte("date", date)
+    .lte("date", getToday({ end: true }));
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
+
+export async function getStaysAfterDate(date) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, clients(fullName)")
+    .gte("date", date)
+    .lte("date", getToday());
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+
+  return data;
+}
+
+export async function getStaysTodayActivity() {
+  const { data, error } = await supabase
+    .from("bookings")
+    .select("*, clients(fullName)")
+    .or(
+      `and(status.eq.unconfirmed,date.eq.${getToday()}),and(status.eq.checked-in,date.eq.${getToday()})`
+    )
+    .order("created_at");
+
+  if (error) {
+    console.error(error);
+    throw new Error("Bookings could not be loaded");
+  }
+  return data;
+}
+
+export async function updateBooking(id, obj) {
+  const { data, error } = await supabase
+    .from("bookings")
+    .update(obj)
+    .eq("id", id)
+    .select()
+    .single();
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be updated");
+  }
+  return data;
+}
+
+export async function deleteBooking(id) {
+  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+  return data;
+}
