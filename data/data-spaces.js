1ba8d89600d5ab482d687bd4f28ff7b9db3379d6
huz3y <huzey4hassan@gmail.com>
2024-08-31 01:25:26 +0500

    change the dummy data

--- data/data-spaces.js
+++ data/data-spaces.js
@@ -33,38 +33,4 @@ export const spaces = [
     unique_features:
       "Additional seating arrangements, Secure Lockers, White Board, TV Screen",
   },
-  {
-    name: "Workspace Zeta",
-    max_capacity: 12,
-    price: 350,
-    unique_features:
-      "Wifi, Video Conferencing Facilities, Kitchenette, Restrooms",
-  },
-  {
-    name: "Workspace Eta",
-    max_capacity: 14,
-    price: 400,
-    unique_features:
-      "Additional seating arrangements, Wifi, Secure Lockers, White Board",
-  },
-  {
-    name: "Workspace Theta",
-    max_capacity: 3,
-    price: 80,
-    unique_features:
-      "TV Screen, Video Conferencing Facilities, Kitchenette, Restrooms",
-  },
-  {
-    name: "Workspace Iota",
-    max_capacity: 5,
-    price: 180,
-    unique_features: "Wifi, Secure Lockers, White Board, TV Screen",
-  },
-  {
-    name: "Workspace Kappa",
-    max_capacity: 7,
-    price: 220,
-    unique_features:
-      "Video Conferencing Facilities, Kitchenette, Restrooms, Wifi",
-  },
 ];
