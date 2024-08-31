5184bf5c2fa700911c1b1507f10edc99fe97f1f1
huz3y <huzey4hassan@gmail.com>
2024-08-31 19:34:36 +0500

    bug fix

--- src/features/authentication/useLogin.js
+++ src/features/authentication/useLogin.js
@@ -11,7 +11,7 @@ export function useLogin() {
     mutationFn: ({ email, password }) => loginApi({ email, password }),
     onSuccess: (user) => {
       queryClient.setQueryData(["user"], user.user);
-      navigate("/dashboard", { replace: true });
+      navigate("/bookings", { replace: true });
     },
     onError: (err) => {
       console.log("ERROR", err);
