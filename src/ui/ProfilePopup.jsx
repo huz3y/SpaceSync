adbe94f7d4fe3bf4e5b7d791d0683c2d103aa1a3
huz3y <huzey4hassan@gmail.com>
2024-08-31 15:45:53 +0500

    profile card styling

--- src/ui/ProfilePopup.jsx
+++ src/ui/ProfilePopup.jsx
@@ -1,12 +1,20 @@
+import { HiOutlineUserCircle, HiOutlineLogout } from "react-icons/hi";
+
 function ProfilePopup() {
   return (
-    <div className="row--horizontal">
+    <div className="profile-popup">
       <img
         src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50"
         alt="Profile"
+        className="profile-popup__image"
       />
-      <p>Username</p>
-      <button>Logout</button>
+      <p className="profile-popup__text">Welcome, user</p>
+      <button className="profile-popup__icon-button">
+        <HiOutlineUserCircle />
+      </button>
+      <button className="profile-popup__logout">
+        <HiOutlineLogout />
+      </button>
     </div>
   );
 }
