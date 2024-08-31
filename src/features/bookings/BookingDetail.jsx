06b1b6db25b2b022b8ac46f36a7f6de8acc0e7ad
huz3y <huzey4hassan@gmail.com>
2024-08-31 10:02:36 +0500

    add delete booking functionality

--- src/features/bookings/BookingDetail.jsx
+++ src/features/bookings/BookingDetail.jsx
@@ -6,9 +6,11 @@ import Button from "../../ui/Button";
 import Spinner from "../../ui/Spinner";
 import { useNavigate } from "react-router-dom";
 import { useBooking } from "./useBooking";
+import { useDeleteBooking } from "./useDeleteBooking";
 
 function BookingDetail() {
   const { booking, isLoading } = useBooking();
+  const { isDeleting, deleteBooking } = useDeleteBooking();
   const navigate = useNavigate();
 
   const statusToTagName = {
@@ -16,10 +18,21 @@ function BookingDetail() {
     booked: "blue",
     completed: "green",
   };
+
   if (isLoading) return <Spinner />;
   if (!booking) return <p className="message">Booking details not found</p>;
 
   const { id, status } = booking;
+
+  const handleDelete = () => {
+    const confirmDeletion = window.confirm(
+      "Are you sure you want to delete this booking? This action cannot be undone."
+    );
+    if (confirmDeletion) {
+      deleteBooking(id);
+      navigate("/bookings");
+    }
+  };
 
   return (
     <>
@@ -33,11 +46,14 @@ function BookingDetail() {
       </div>
 
       <BookingDataBox booking={booking} />
+
       <ButtonGroup className="booking-detail-button-group">
         <Button variation="secondary" extra="back" onClick={() => navigate(-1)}>
           Back
         </Button>
-        <Button variation="danger">Delete Booking</Button>
+        <Button variation="danger" onClick={handleDelete} disabled={isDeleting}>
+          Delete Booking
+        </Button>
         <Button
           variation="primary"
           disabled={status !== "unconfirmed"}
---
+++ src/features/bookings/useDeleteBooking.js
@@ -0,0 +1,21 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { toast } from "react-hot-toast";
+import { deleteBooking as deleteBookingApi } from "../../services/apiBookings";
+
+export function useDeleteBooking() {
+  const queryClient = useQueryClient();
+
+  const { isLoading: isDeleting, mutate: deleteBooking } = useMutation({
+    mutationFn: deleteBookingApi,
+    onSuccess: () => {
+      toast.success("Booking has been deleted 😊");
+
+      queryClient.invalidateQueries({
+        queryKey: ["bookings"],
+      });
+    },
+    onError: (err) => toast.error(err.message),
+  });
+
+  return { isDeleting, deleteBooking };
+}
